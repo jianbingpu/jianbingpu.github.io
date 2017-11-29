ab6bb25fad6555054eaf967f8737f45eae76a2d5
jianbingpu <jbp2017@sina.com>
2017-11-29 15:46:18 +0800

    upd project

--- js/jbp.js
+++ js/jbp.js
@@ -53,3 +53,14 @@
 	      $(this).toggleClass(animateStr);
 	    });
 	}
+	
+	
+	function jump(url) {
+	    var a = document.createElement('a');
+	    a.setAttribute('rel', 'noreferrer');
+	    a.setAttribute('id', 'hideClick');
+	    a.setAttribute('href', url);
+	    document.body.appendChild(a);
+	    document.getElementById('hideClick').click();
+	    document.body.removeChild(a);
+	}
