6aff0a93e4480f268de9253778c50a965d4f8a1e
jianbingpu <jbp2017@sina.com>
2017-12-23 03:53:01 +0800

    upd project

--- js/jbp.js
+++ js/jbp.js
@@ -125,30 +125,3 @@
 	    return false;
 	};
 	
-	
-	function resizeimg(obj,maxW,maxH)
-	{
-	         var imgW=obj.width;
-	         var imgH=obj.height;
-
-	         if(imgW>maxW||imgH>maxH)
-	         {       
-	                  var ratioA=imgW/maxW;
-	                  var ratioB=imgH/maxH;               
-	                  if(ratioA>ratioB)
-	                  {
-	                           imgW=maxW;
-	                           imgH=maxW*(imgH/imgW);
-	                  }
-	                  else
-	                  {
-	                           imgH=maxH;
-	                           imgW=maxH*(imgW/imgH);
-	                  }  
-	                  obj.width=imgW;
-	                  obj.height=imgH;
-	         }
-	         
-	         obj.style.margin-left=(maxW-imgW)/2;
-			 obj.style.margin-top=(maxH-imgH)/2;
-	}
