450551056fdcaddbe25d06dce9b0c7dcd06275ec
jianbingpu <jbp2017@sina.com>
2019-01-09 22:47:56 +0800

    更新

--- js/jbp.js
+++ js/jbp.js
@@ -9,7 +9,7 @@
 	}
 	
 	function back() {
-		this.location.href="gj.html?v=190111";
+		this.location.href="gj.html?v=190112";
 	}
 
 	function tip(msg) {
@@ -166,7 +166,7 @@
         if(!window.isPrivateMode && !!window.localStorage){
             localStorage.setItem("jbp_tool_srcollTop", document.body.scrollTop);
         }
-        jump(page+"?v=190111");
+        jump(page+"?v=190112");
         //this.location.href = page;
     }
 
