6910c0e420890d9ff9e64bc7d7cf96976ddcbb33
jianbingpu <jbp2017@sina.com>
2021-01-22 10:27:18 +0800

    upd

--- js/jbp.js
+++ js/jbp.js
@@ -9,7 +9,7 @@
 	}
 	
 	function back() {
-		this.location.href="gj.html?v=201009";
+		this.location.href="gj.html?v=210122";
 	}
 
 	function tip(msg) {
@@ -166,7 +166,7 @@
         if(!window.isPrivateMode && !!window.localStorage){
             localStorage.setItem("jbp_tool_srcollTop", document.body.scrollTop);
         }
-        jump(page+"?v=201009");
+        jump(page+"?v=210122");
         //this.location.href = page;
     }
 
